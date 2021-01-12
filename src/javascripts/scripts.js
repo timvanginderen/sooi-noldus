71222de41c1bbb59e6fbf3bd8d89776d68e3b789
Tim Van Ginderen <timvanginderen@gmail.com>
2021-01-12 04:34:22 +0100

    Load bootstrap scss and js via npm WIP

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -1,1 +1,12 @@
 // Add your scripts here
+// import 'bootstrap';
+// require('bootstrap/dist/js/bootstrap');
+// import 'bootstrap/dist/js/bootstrap';
+
+import 'bootstrap';
+
+// import '/bootstrap/dist/js/*.js';
+
+// import 'bootstrap/js/dist/*.js';
+
+alert('joep');
