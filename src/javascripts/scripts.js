e26b9fff8d86f518a3b7db7928b8bf00c7f0605a
Tim Van Ginderen <timvanginderen@gmail.com>
2021-01-12 04:44:32 +0100

    Cleanup

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -1,12 +1,2 @@
 // Add your scripts here
-// import 'bootstrap';
-// require('bootstrap/dist/js/bootstrap');
-// import 'bootstrap/dist/js/bootstrap';
-
 import 'bootstrap';
-
-// import '/bootstrap/dist/js/*.js';
-
-// import 'bootstrap/js/dist/*.js';
-
-alert('joep');
