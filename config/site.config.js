5ea14adef2ec95715d8a77a02cf9fd1fc5461406
Tim Van Ginderen <timvanginderen@gmail.com>
2021-02-16 18:38:27 +0100

    Add google analytics tracking id

--- config/site.config.js
+++ config/site.config.js
@@ -18,7 +18,7 @@ const config = {
   site_url: 'http://www.sooinoldus.be',
 
   // Google Analytics tracking ID (leave blank to disable)
-  googleAnalyticsUA: '',
+  googleAnalyticsUA: 'G-M1BN650131',
 
   // The viewport meta tag added to your HTML page's <head> tag
   viewport: 'width=device-width,initial-scale=1',
