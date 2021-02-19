2a00cd9971e9f9f4666586881c13e4ddd5fa38e3
Tim Van Ginderen <timvanginderen@gmail.com>
2021-02-19 15:26:12 +0100

    Update deploy script

--- deploy_githubpages.sh
+++ deploy_githubpages.sh
@@ -3,6 +3,8 @@ echo "building dist..."
 npm run build:dist
 echo "copy js files..."
 cp -R src/javascripts/ dist/javascripts/
+echo "copy binary files..."
+cp -R src/videos/ dist/videos/
 #echo "pushing changes to gh-pages branch..."
 #todo add an commit changes to dist dir
 #git subtree push --prefix dist origin gh-pages
